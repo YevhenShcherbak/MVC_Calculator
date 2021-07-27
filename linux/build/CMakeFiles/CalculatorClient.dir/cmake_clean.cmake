2f7057e564cfdc96023e5fdb0a97255128d85254
Yevhen Shcherbak <35167188+YevhenShcherbak@users.noreply.github.com>
2021-07-27 14:19:51 +0300

    Add  unfinished build with CMake

---
+++ linux/build/CMakeFiles/CalculatorClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculatorClient.dir/CalculatorClient.cpp.o"
+  "CalculatorClient"
+  "CalculatorClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalculatorClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ linux/build/CMakeFiles/CalculatorServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculatorServer.dir/CalculatorServer.cpp.o"
+  "CMakeFiles/CalculatorServer.dir/src/Calculator.cpp.o"
+  "CMakeFiles/CalculatorServer.dir/src/CalculatorController.cpp.o"
+  "CMakeFiles/CalculatorServer.dir/src/CalculatorView.cpp.o"
+  "CalculatorServer"
+  "CalculatorServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalculatorServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
