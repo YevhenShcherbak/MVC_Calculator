c184e7aab93058f93f6d00b588dafb1bdc95dab7
Yevhen Shcherbak <35167188+YevhenShcherbak@users.noreply.github.com>
2021-07-29 16:23:01 +0300

    Add builded files with cmake

---
+++ linux/CMakeFiles/CalculatorServer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/CalculatorServer.cpp" "/mnt/build/CMakeFiles/CalculatorServer.dir/CalculatorServer.cpp.o"
+  "/mnt/src/Calculator.cpp" "/mnt/build/CMakeFiles/CalculatorServer.dir/src/Calculator.cpp.o"
+  "/mnt/src/CalculatorController.cpp" "/mnt/build/CMakeFiles/CalculatorServer.dir/src/CalculatorController.cpp.o"
+  "/mnt/src/CalculatorView.cpp" "/mnt/build/CMakeFiles/CalculatorServer.dir/src/CalculatorView.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../MVC_Calculator"
+  "../C:/Users/Evgeny/Desktop/MVC_Calculator/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
