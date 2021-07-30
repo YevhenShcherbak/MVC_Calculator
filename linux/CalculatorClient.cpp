89cf72f67c74c1bd6c037cbbb05c1b819ad7fccd
Yevhen Shcherbak <35167188+YevhenShcherbak@users.noreply.github.com>
2021-07-30 21:20:01 +0300

    Update CalculatorClient
    Add while loop and exit from programm

--- linux/CalculatorClient.cpp
+++ linux/CalculatorClient.cpp
@@ -13,46 +13,54 @@ bool Send()
 {
 	int shm;
 	int mode = O_CREAT;
-	char* addr;
-	std::string expression;
-	std::cout << "Write your expression: ";
+	char *addr;
+	std::string expression = "0";
+	std::cout << "Write your expression or 'exit' to quit programm: ";
 	std::getline(std::cin, expression);
-
-	if ((shm = shm_open(MEMORY_NAME, mode | O_RDWR, 0777)) == -1)
+	while (expression.find("exit") == std::string::npos)
 	{
-		perror("shm_open");
-		return 1;
+
+		if ((shm = shm_open(MEMORY_NAME, mode | O_RDWR, 0777)) == -1)
+		{
+			perror("shm_open");
+			return 1;
+		}
+
+		if (ftruncate(shm, BUFFER_SIZE + 1) == -1)
+		{
+			perror("ftruncate");
+			return 1;
+		}
+
+		addr = (char *)mmap(0, BUFFER_SIZE + 1, PROT_WRITE | PROT_READ, MAP_SHARED, shm, 0);
+		if (addr == (char *)-1)
+		{
+			perror("mmap");
+			return 1;
+		}
+
+		memcpy(addr, expression.c_str(), BUFFER_SIZE);
+		std::cout << "Shared memory successfull filled with your expression!" << std::endl;
+
+		munmap(addr, BUFFER_SIZE);
+		close(shm);
+
+		std::cout << "Write your expression or exit to quit programm: ";
+		std::getline(std::cin, expression);
 	}
-
-	if (ftruncate(shm, BUFFER_SIZE + 1) == -1)
-	{
-		perror("ftruncate");
-		return 1;
-	}
-
-	addr = (char *)mmap(0, BUFFER_SIZE + 1, PROT_WRITE | PROT_READ, MAP_SHARED, shm, 0);
-	if (addr == (char *)-1)
-	{
-		perror("mmap");
-		return 1;
-	}
-
-	memcpy(addr, expression.c_str(), BUFFER_SIZE);
-	std::cout << "Shared memory successfull filled with your expression!" << std::endl;
-
-	munmap(addr, BUFFER_SIZE);
-	close(shm);
-
-	std::string temp;
-	std::cin >> temp;
-
 	shm_unlink(MEMORY_NAME);
-
 	return true;
 }
 
 int main()
 {
-	Send();
+	if (Send())
+	{
+		std::cout << "Exiting programm" << std::endl;
+	}
+	else
+	{
+		std::cout << "Programm ended with error" << std::endl;
+	}
 	return 1;
 }
